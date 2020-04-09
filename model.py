8d856bd771327aa9785ecbc6b8f17abc4c6574b8
Muammad Shehab <muhammad.a.shehab@gmail.com>
2020-04-09 18:59:33 +0200

    Add files via upload

---
+++ model.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        "*** YOUR CODE HERE ***"
+        fc1Out_fc2In = 64
+        fc2Out_fc3In = 64
+        fc3Out_SoftIn = 32
+        self.fc1 = nn.Linear(state_size,fc1Out_fc2In)
+        self.fc2 = nn.Linear(fc1Out_fc2In,fc2Out_fc3In)
+        self.fc3 = nn.Linear(fc2Out_fc3In,fc3Out_SoftIn)
+        self.fc4 = nn.Linear(fc3Out_SoftIn,action_size)
+        
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        #x = F.softmax(x,dim=1)
+        return x
